accb59be459bdb89f8651b2f10ad2ad52c9369e2
emilyTheseond <esegundo02@gmail.com>
2024-04-18 11:01:07 -0500

    include logo png

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { NavHashLink } from 'react-router-hash-link'
 
 
+
 const Nav = () => {
   return (
     <div>
         <div>
+            <NavHashLink to='#home'>
+                <img src='/public/cleaning.png' alt="cleaning logo" />
+            </NavHashLink>
             <NavHashLink to='#about'>About Us</NavHashLink>
             <NavHashLink to='#services'>Services</NavHashLink>
             <NavHashLink to='#faq'>FAQ</NavHashLink>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='page' id='home' >
+      
+    </div>
+  )
+}
+
+export default Home
