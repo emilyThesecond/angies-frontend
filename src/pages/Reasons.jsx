9c780b6507972bf7bc243d7fb708bf72dad89655
emilyTheseond <esegundo02@gmail.com>
2024-05-02 13:33:27 -0500

    include images in reasons component

--- src/pages/Reasons.jsx
+++ src/pages/Reasons.jsx
@@ -6,12 +6,15 @@ const Reasons = () => {
       <h1>WHY CHOOSE US?</h1>
       <div>
         <div>
+          <img src="https://angiescleaningservice4.files.wordpress.com/2024/04/house-cleaning-8373938_1280.jpg?w=1024" alt="Cleaning Crew" />
            <h1>Certified professionals with 20+ experince</h1>
         </div>
         <div>
+          <img src="https://organicboosting.bio/wp-content/uploads/2022/04/History-evolution-and-characteristics-of-ecological-cleaning-products.jpg" alt="" />
             <h1>Environmentally Friendly Cleaning</h1>
         </div>
         <div>
+          <img src="https://images.ctfassets.net/f60q1anpxzid/asset-50b24820d2a79b30d091c61307acf32d/9b7392a1de285785c22e61d829395f6d/dog-photo.jpg?fm=jpg&fl=progressive&q=50&w=1200" alt="" />
             <h1>Non-toxic products</h1>
             <p>You dont have to worrry, none of the products we use iwill harm you, your family including your furry friends.</p>
         </div>
