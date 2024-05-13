fcf66bd704165be38fe6920781a2c48c1a9b6d8a
emilyTheseond <esegundo02@gmail.com>
2024-05-13 14:21:37 -0500

    css

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,6 +4,7 @@ const Logo = () => {
   return (
     <div className='logo-middle'>
         <img className="logo" src="/logoclear.jpg" alt="" />
+        {/* <img className="logo" src="/ANGIE'S CLEANING SERVICES (4).png" alt="" /> */}
       <h1>Angie's <br/>Cleaning<br/> Service</h1>
     </div>
   )
